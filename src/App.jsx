01170cb426a778dd9e058283398d0c37230b16ee
lavanyasingh12 <lavanyasinghh1206@gmail.com>
2026-06-30 13:47:39 +0530

    Initial Hall of Fame setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        fontSize: "3rem",
+      }}
+    >
+      Hall of Fame
+    </div>
+  );
+}
+
+export default App;
